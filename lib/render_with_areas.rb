7f5ab942f83e10ccda90d18e4419233132f75c90
Jens Balvig <jens@balvig.com>
2023-03-27 19:18:30 +0900

    remember to require

--- lib/render_with_areas.rb
+++ lib/render_with_areas.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "render_with_areas/version"
+require_relative "render_with_areas/content_areas"
 
 module RenderWithAreas
   def render_with_areas(*args, &block)
